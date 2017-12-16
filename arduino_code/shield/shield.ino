bc28bb19e097f2fcf2338fb5b739e3911610cbc7
Ray Suhyun Lee <raysuhyunlee@gmail.com>
2017-12-16 13:22:42 +0900

    Modify source code

--- arduino_code/shield/shield.ino
+++ arduino_code/shield/shield.ino
@@ -1,32 +1,54 @@
+/*
+ * shield.ino
+ *
+ * Arduino Uno shiled source code
+ *
+ * read 6 potentiometor values and
+ * control each servo
+ *
+ * Maintainer: Ray Suhyun Lee(raysuhyunlee@gmail.com)
+ */
+
 #include <Servo.h>
 
 #define SERVO_NUM 6
 #define POTEN_NUM 6
+
+/* 
+ * you may modify these two values
+ * if your servos use different pwm range
+ */
+#define PWM_MIN 1000
+#define PWM_MAX 2000
+
+#define POTEN_MARGIN 250
+
 Servo servos[SERVO_NUM];
 unsigned char potens[POTEN_NUM] = {
   A0, A1, A2, A3, A4, A5
 };
 
 void setup() {
-  // put your setup code here, to run once:
+  /* attach digital pin 2~7 to each servo */
   for (int i = 0; i < SERVO_NUM; i++) {
     servos[i].attach(2 + i);
     delay(20);
   }
-  Serial.begin(9600);
 }
 
 void loop() {
-  Serial.println(analogRead(A0));
-  // put your main code here, to run repeatedly:
   unsigned short poten_values[POTEN_NUM];
+
+  /* read each potentiometor value */
   for (int i=0; i<POTEN_NUM; i++) {
-    unsigned short tmp = analogRead(potens[i]);
-    poten_values[i] = map(tmp, 0+250, 1024-250, 1000, 2000);
-    poten_values[i] = constrain(poten_values[i], 1000, 2000);
+    poten_values[i] = analogRead(potens[i]);
   }
 
+  /* send angle signal to each servo */
   for (int i=0; i<SERVO_NUM; i++) {
-    servos[i].writeMicroseconds(poten_values[i]);
+    unsigned short buf = map(poten_values[i], 
+        0 + POTEN_MARGIN, 1024 - POTEN_MARGIN, PWM_MIN, PWM_MAX);
+    buf = constrain(buf, PWM_MIN, PWM_MAX);
+    servos[i].writeMicroseconds(buf);
   }
 }
